5e3f32e48cb3ef9c6ef7b264a8f0300fc64ef9c7
linlianhui <linlianhui@email.com>
2020-02-05 17:03:14 +0800

    “‘llh-StockController'"

---
+++ TongManage/TongManage/Services/StockService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TongManage.Daos;
+using TongManage.Models;
+
+namespace TongManage.Services
+{
+    public class StockService
+    {
+        private static InventoryRecordDao inventoryRecordDao  = new InventoryRecordDao();
+
+        /// <summary>
+        /// 创建一条进出库记录
+        /// </summary>
+        /// 
+        /// <param name="inventoryRecord">进出库记录类</param>
+        /// <returns>操作状态码</returns>
+        public int CreateFixtureRecord(InventoryRecord inventoryRecord)
+        {
+            return inventoryRecordDao.insertInventoryRecord(inventoryRecord);
+        }
+
+        /// <summary>
+        /// 通过ID查找对应的进出库记录
+        /// </summary>
+        /// 
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public InventoryRecord GetRecordDetailById(int id)
+        {
+            return inventoryRecordDao.selectInventoryRecordById(id);
+        }
+
+        /// <summary>
+        /// 通过ID删除对应的进出库记录
+        /// </summary>
+        /// 
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public int DeleteRecordById(int id)
+        {
+            return inventoryRecordDao.deleteInventoryRecordById(id);
+        }
+    }
+}
